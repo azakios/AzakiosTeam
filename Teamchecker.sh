7d261e31812686ca2ac9a025c4f13611bea0a78b
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-09 23:51:27 +0530

    Create Teamchecker.sh

---
+++ Teamchecker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter Your Name : " 
+read Name
+echo "Welcome $Name Azakios Page"
+if [ $Name=Aafaq ]
+echo "Hello Mir Aafaq,Nice To See U Sir"
+else 
+  echo"welcome Dear User"
+  fi
