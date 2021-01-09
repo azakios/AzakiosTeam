0c3f2b8358fb36cf7ea920c49a5552cbea218a14
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-09 23:56:03 +0530

    Update Teamchecker.sh

--- Teamchecker.sh
+++ Teamchecker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Enter Your Name : " 
+echo "Enter Your Name : \c" 
 read Name
 echo "Welcome $Name Azakios Page"
-if [ $Name=Aafaq ]
+if [ $Name=Aafaq ] then
 echo "Hello Mir Aafaq,Nice To See U Sir"
 else 
   echo"welcome Dear User"
