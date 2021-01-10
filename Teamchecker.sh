b3f96f21a2400429525a9b814d45678ddf130e1a
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-11 00:00:28 +0530

    Update Teamchecker.sh

--- Teamchecker.sh
+++ Teamchecker.sh
@@ -5,48 +5,34 @@ banner()
   echo "+------------------------------------------+"
   printf "| %-40s |\n" "`date`"
   echo "|                                          |"
-  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  printf "|`Aza`|\n" "$@"
   echo "+------------------------------------------+"
 }
 
-banner "Starting the Job"
-sleep 3
 
-banner "Copying files"
-cp -v /etc/hosts /tmp
-cp -v /etc/passwd /tmp
-sleep 4
-
-banner "Downloading article"
-curl https://www.shellscript.sh/tips/banner/ > /tmp/banner.html
-sleep 5
-
-banner "Finished."
-banner() {
-
-clear
-printf "      \e[33;1m••••••••••••••••••••••••••••••••••••\e[0m\n"
-printf "      \e[33;1m°°°|||||||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°|||||||||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°||_____||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°||/////||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°||     ||ZAKIOS®°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°°°°°°A++++++++A°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°°°°°°A°°°°°°°°C°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°°°°°°F°°°°°°°°E°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°°°°°°A°°°°°°°°F°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m°°°°°°°Q°°°°°°°°I°°°°°°°°°°°°°°°°°°°\e[0m\n"
-printf "      \e[33;1m__________[NEW E®A]_________________\e[0m\n"
-printf "      \e[32;1m ©2020Azakios.All Rights Reserved___\e[0m\n"
-printf "      \e[32;1m •MIR AAFAQ •ACEJAFIIMACE •MIR MUSHAHID\e[0m\n"
-printf "      \e[32;1m BE LIKE A DIAMOND WHICH IS FOUND RARE,NOT A STONE FOUND EVERYWHERE \e[0m\n"
-printf "      \e[33;1m AZAKIOS-THE TECH BASED ORGANISATION\e[0m\n"
+printf "      |\e[33;1m••••••••••••••••••••••••••••••••••••\e[0m\n|"
+printf "      |\e[33;1m°°°|||||||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°|||||||||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°||_____||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°||/////||°°°°°°°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°||     ||ZAKIOS®°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°°°°°°A++++++++A°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°°°°°°A°°°°°°°°C°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°°°°°°F°°°°°°°°E°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°°°°°°A°°°°°°°°F°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m°°°°°°°Q°°°°°°°°I°°°°°°°°°°°°°°°°°°°\e[0m\n|"
+printf "      |\e[33;1m__________[NEW E®A]_________________\e[0m\n|"
+printf "      |\e[32;1m ©2020Azakios.All Rights Reserved___\e[0m\n|"
+printf "      |\e[32;1m •MIR AAFAQ •ACEJAFIIMACE •MIR MUSHAHID\e[0m\n|"
+printf "      |\e[32;1m BE LIKE A DIAMOND WHICH IS FOUND RARE,NOT A STONE FOUND EVERYWHERE \e[0m\n|"
+printf "      |\e[33;1m AZAKIOS-THE TECH BASED ORGANISATION\e[0m\n|"
 printf "      \e[33;1m                \e[1;97m    Version 0.01 Alpha®      \e[0m\n"
 printf "\n"
 printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Coded & Scripted  by Azakios@(mir.aafaq)\e[0m\n"
 printf "\n"
 
 }
+banner "Finished."
 
 string_a=Aafaq
 echo 'Enter Your Name : \c' 
