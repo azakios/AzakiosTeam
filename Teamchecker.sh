5aa6111b9133a76b8688de117652a04b37b6a4be
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-10 22:40:26 +0530

    Update Teamchecker.sh

--- Teamchecker.sh
+++ Teamchecker.sh
@@ -1,21 +1,10 @@
 #! bin/bash
-echo "_              _    _"
-  echo" / \    ______ _| | _(_) ___  ___"
-  echo"/ _ \  |_  / _` | |/ / |/ _ \/ __|"
-echo" / ___ \  / / (_| |   <| | (_) \__ \"
-echo"/_/   \_\/___\__,_|_|\_\_|\___/|___/"
 string_a=Aafaq
 echo 'Enter Your Name : \c ' 
 read string_b
 if [[ $string_a == $string_b ]]; then
   echo 'Welcome Azakios Sir'
-echo '__  __ _           _          __'
-echo'|  \/  (_)_ __     / \   __ _ / _| __ _  __ _'
-echo'| |\/| | | '__|   / _ \ / _` | |_ / _` |/ _` |'
-echo'| |  | | | |     / ___ \ (_| |  _| (_| | (_| |'
-echo'|_|  |_|_|_|    /_/   \_\__,_|_|  \__,_|\__, |'
-                         echo'                  |_|'
 else
   echo 'Welcome Dear User'
 fi
-echo ''
+echo 'TAKE CARE'
