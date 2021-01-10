59504dfec2243bb2aaebfa7d1ce95fc5272963a5
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-10 20:57:47 +0530

    Update Teamchecker.sh

--- Teamchecker.sh
+++ Teamchecker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-NAME=Aafaq
- read -p "Enter Your nickname : " nickname
-read $nickname
-echo "welcome $nickname"
-echo "smile"
-if [[ "$NAME"=="$nickname" ]] then
 
-echo "Hello Mr Aafaq,Nice To See U Sir"
+ read -p "Enter Your nickname : " nickname
+echo "welcome $nickname"
+if [[ "$nickname"=="Aafaq" ]] 
+then
+ echo "Hello Mr Aafaq,Nice To See U Sir"
 else 
   echo "welcome Dear User"
   fi
